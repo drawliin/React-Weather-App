93b666e8e2866f41853cc3ab613f4e0e3c7f9a9f
Houssam Hamouich <itshoussameddine@gmail.com>
2024-12-26 02:12:02 +0100

    remove unused files and components; add SearchEngine component

---
+++ src/components/SearchEngine.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function SearchEngine() {
+  return (
+    <div>
+        <div>
+            <input type='text' placeholder='Enter City Name...' />
+            <button>Search</button>
+        </div>
+    </div>
+  )
+}
+
+export default SearchEngine
